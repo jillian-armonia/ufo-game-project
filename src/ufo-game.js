8ee6b830aee5ff019996e8e6fbe4aa78c8883e2c
Jillian Armonia <jillian.marice@gmail.com>
2024-03-27 18:20:10 +0900

    added sounds

--- src/ufo-game.js
+++ src/ufo-game.js
@@ -27,6 +27,70 @@ const words = [
     "WRESTLING",
 ];
 
+const UFO_AUDIO = [
+    {
+        id: "ufo-audio-0",
+        src: "https://sampleswap.com/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Atmospherics/1421[kb]alien-scene.wav.mp3"
+    },
+
+    {
+        id: "ufo-audio-1",
+        src: "https://sampleswap.com/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Pads/3100[kb]alien-monolith-pad.wav.mp3"
+    },
+
+    {
+        id: "ufo-audio-2",
+        src: "https://sampleswap.com/samples-ghost/DRUMS%20(SINGLE%20HITS)/Weird%20and%20Interesting%20Hits/88[kb]alien_alarm.wav.mp3"
+    },
+
+    {
+        id: "ufo-audio-3",
+        src: "https://sampleswap.com/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Electro%20and%20Synthetic/348[kb]alien-weapon-powerup.wav.mp3"
+    },
+
+    {
+        id: "ufo-audio-4",
+        src: "https://sampleswap.com/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Scars%20FNC%20SFX/92[kb]alien_breath.wav.mp3"
+    },
+
+    {
+        id: "ufo-audio-5",
+        src: "https://sampleswap.com/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Scars%20FNC%20SFX/92[kb]alien_breath.wav.mp3"
+    },
+
+    {
+        id: "ufo-audio-6",
+        src: "https://sampleswap.com/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Atmospherics/1298[kb]alien-propeller.wav.mp3"
+    },
+
+    {
+        id: "ufo-audio-7",
+        src: "https://sampleswap.com/samples-ghost/VOCALS%20and%20SPOKEN%20WORD/Robotic%20or%20Stuttery%20or%20Spooky/207[kb]aliens-laughing.wav.mp3"
+    },
+
+    {
+        id: "game-over-audio",
+        src: "https://sampleswap.com/samples-ghost/MELODIC%20SAMPLES/SAMPLED%20MUSIC/676[kb]close-encounters-alien-bass-response.wav.mp3"
+    },
+
+    {
+        id: "you-win-audio",
+        src: "https://sampleswap.com/samples-ghost/MELODIC%20LOOPS/SYNTH%20AND%20ELECTRONIC%20LOOPS/380[kb]119_round-happy-acid-synth.wav.mp3"
+    }
+];
+
+const BUTTONS_AUDIO = [
+    {
+        id: "wrong",
+        src: "https://sampleswap.com/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/50[kb]somebuzz-kick.wav.mp3"
+    },
+
+    {
+        id: "correct",
+        src: "https://sampleswap.com/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/122[kb]pipechimes.wav.mp3"
+    }
+];
+
 const maxWrongGuesses = 8;
 let wordToGuess = "";
 let guessedLetters =[];
@@ -111,6 +175,11 @@ function handleGuess(letter){
     if (!wordToGuess.includes(letter)){
         wrongGuesses++;
         updateUFOGraphic();
+    } else {
+        const audio = document.createElement("audio");
+        audio.setAttribute("src", "https://sampleswap.com/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/122[kb]pipechimes.wav.mp3");
+        audio.setAttribute("autoplay", "true");
+        audio.volume = 0.1;
     }
 
     //Update the word display
@@ -122,19 +191,26 @@ function handleGuess(letter){
 
 function updateUFOGraphic(){
     const UFOContainer = document.querySelector('.UFO');
-    UFOContainer.innerHTML = `<img src="../images/ufo-gifs/ufo-${imageCount}.gif" alt="UFO ${imageCount}">`;
+    UFOContainer.innerHTML = `<img src="../images/ufo-gifs/ufo-${imageCount}.gif" alt="UFO ${imageCount}"><audio id="ufo-audio-${imageCount}" src=${UFO_AUDIO[imageCount].src} autoplay></audio>`;
+
+    let sound = document.querySelector("audio");
+    sound.volume = 0.10;
+
 
     //Also update the guess counter
     const counterContainer = document.querySelector('.guess-counter');
     counterContainer.innerText = `You have ${maxWrongGuesses - imageCount} chances left.`;
     imageCount++;
-}
+};
 
 function checkWinOrLose(){
     if (guessedLetters.join('') === wordToGuess){
         const UFOContainer = document.querySelector('.UFO');
-        UFOContainer.innerHTML = `<img src="../images/ufo-gifs/you-win.gif" alt="You Win">`;
+        UFOContainer.innerHTML = `<img src="../images/ufo-gifs/you-win.gif" alt="You Win"><audio id="you-win-audio" src=${UFO_AUDIO[UFO_AUDIO.length - 1].src} autoplay></audio>`;
         UFOContainer.classList.add("win");
+
+        let sound = document.querySelector("audio");
+        sound.volume = 0.10;
 
         const counterContainer = document.querySelector('.guess-counter');
         counterContainer.innerText = "";
@@ -154,7 +230,10 @@ function checkWinOrLose(){
 
     } else if(wrongGuesses >= maxWrongGuesses){
         const UFOContainer = document.querySelector('.UFO');
-        UFOContainer.innerHTML = `<img src="../images/ufo-gifs/game-over.gif" alt="Game Over">`;
+        UFOContainer.innerHTML = `<img src="../images/ufo-gifs/game-over.gif" alt="Game Over"><audio id="game-over-audio" src=${UFO_AUDIO[8].src} autoplay></audio>`;
+
+        let sound = document.querySelector("audio");
+        sound.volume = 0.10;
 
         //Shows the answer by filling in the blanks
         guessedLetters = wordToGuess.split('');
